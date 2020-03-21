99fd96a64e75a3c5ffffea77fc9718d6e4270948
joachimprinzbach <joachim.prinzbach@gmail.com>
2020-03-21 18:20:39 +0100

    Add the amazing ui

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker build pizza-inventory -t joachimprinzbach/pizza-inventory:1
-docker build pizza-order -t joachimprinzbach/pizza-order:1
+docker build pizza-inventory -t joachimprinzbach/pizza-inventory:3
+docker build pizza-order -t joachimprinzbach/pizza-order:3
--- docker-push.sh
+++ docker-push.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker push joachimprinzbach/pizza-inventory:1
-docker push joachimprinzbach/pizza-order:1
+docker push joachimprinzbach/pizza-inventory:3
+docker push joachimprinzbach/pizza-order:3
