0f7afae6d08844861dbb97bd44955fff000eb2b9
joachimprinzbach <joachim.prinzbach@gmail.com>
2020-03-21 18:35:09 +0100

    Fix service url

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker build pizza-inventory -t joachimprinzbach/pizza-inventory:3
-docker build pizza-order -t joachimprinzbach/pizza-order:3
+docker build pizza-inventory -t joachimprinzbach/pizza-inventory:4
+docker build pizza-order -t joachimprinzbach/pizza-order:4
--- docker-push.sh
+++ docker-push.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker push joachimprinzbach/pizza-inventory:3
-docker push joachimprinzbach/pizza-order:3
+docker push joachimprinzbach/pizza-inventory:4
+docker push joachimprinzbach/pizza-order:4
