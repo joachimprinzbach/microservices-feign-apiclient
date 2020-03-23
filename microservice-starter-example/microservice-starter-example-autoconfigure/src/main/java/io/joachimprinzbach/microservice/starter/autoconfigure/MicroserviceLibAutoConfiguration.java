d8120fb3a00739327729486727e31901ef5b40be
joachimprinzbach <joachim.prinzbach@gmail.com>
2020-03-23 14:29:06 +0100

    Add starter skeleton

---
+++ microservice-starter-example/microservice-starter-example-autoconfigure/src/main/java/io/joachimprinzbach/microservice/starter/autoconfigure/MicroserviceLibAutoConfiguration.java
@@ -0,0 +1,8 @@
+package io.joachimprinzbach.microservice.starter.autoconfigure;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MicroserviceLibAutoConfiguration {
+
+}
