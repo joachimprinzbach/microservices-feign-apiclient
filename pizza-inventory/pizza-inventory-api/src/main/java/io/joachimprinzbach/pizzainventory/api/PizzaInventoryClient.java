b5caeb3960e8c6a2a3d565bd3a90109f0488876e
joachimprinzbach <joachim.prinzbach@gmail.com>
2020-03-24 10:14:20 +0100

    Add swagger

--- pizza-inventory/pizza-inventory-api/src/main/java/io/joachimprinzbach/pizzainventory/api/PizzaInventoryClient.java
+++ pizza-inventory/pizza-inventory-api/src/main/java/io/joachimprinzbach/pizzainventory/api/PizzaInventoryClient.java
@@ -1,11 +1,13 @@
 package io.joachimprinzbach.pizzainventory.api;
 
+import io.swagger.annotations.Api;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Set;
 
 @FeignClient(name = "pizza-inventory", url = "${pizza.inventory.url:http://pizza-inventory-service.pizza-inventory-${env}}/")
+@Api("Pizza Inventory API")
 public interface PizzaInventoryClient {
 
     @GetMapping("/api/pizza/items")
--- pizza-inventory/pizza-inventory-api/src/main/java/io/joachimprinzbach/pizzainventory/api/PizzaInventoryItemDto.java
+++ pizza-inventory/pizza-inventory-api/src/main/java/io/joachimprinzbach/pizzainventory/api/PizzaInventoryItemDto.java
@@ -1,5 +1,6 @@
 package io.joachimprinzbach.pizzainventory.api;
 
+import io.swagger.annotations.ApiModel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ApiModel("Pizza Inventory Item Dto")
 public class PizzaInventoryItemDto {
 
     private String name;
